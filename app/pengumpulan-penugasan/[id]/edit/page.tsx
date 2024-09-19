55eeda5504f464636714c66a12742f328e761c4f
xlocaste <irfandi.di122@gmail.com>
2024-09-19 17:32:31 +0700

    Tinggal membuat PopUp Edit

--- app/pengumpulan-penugasan/[id]/edit/page.tsx
+++ app/pengumpulan-penugasan/[id]/edit/page.tsx
@@ -15,11 +15,11 @@ const Edit = () => {
   const [catatan, setCatatan] = useState("");
   const [selectedStatus, setSelectedStatus] = useState<StatusEnum>(StatusEnum.BARU);
   const [penugasanId, setPenugasanId] = useState<string | null>(null); 
-  // const [pengumpualnPenugasan, setPengumpulanPenugasan] = useState({  })
   const [userRole, setUserRole] = useState<string | null>(null);
   const token = Cookies.get("token");
   const params = useParams();
   const router = useRouter();
+  // const [pengumpualnPenugasan, setPengumpulanPenugasan] = useState({  })
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -141,11 +141,11 @@ const Edit = () => {
                     ))}
                   </select>
                 </div>
-          )}
+                )}
             <button
               type="submit"
               className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
+              >
               Update
             </button>
           </form>
--- app/pengumpulan-penugasan/page.tsx
+++ app/pengumpulan-penugasan/page.tsx
@@ -2,25 +2,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import Link from "next/link";
 
-interface penugasan {
-  id: number;
-  nama_laporan: string;
-  code: number;
-  deadline: string;
-  keterangan: string;
-}
-
-interface pengumpulanPenugasan {
-  id: number;
-  penugasan_id: string;
-  link_google_drive: string;
-  user_id: string;
-  catatan: string;
-  status: string;
-  penugasan: {
+interface Penugasan {
     id: number;
     nama_laporan: string;
     code: number;
@@ -29,22 +14,42 @@ interface pengumpulanPenugasan {
     created_at: string;
     updated_at: string;
   };
-  user: {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
-    password: string;
-    remember_token: string;
-    role: string;
-    created_at: string;
-    updated_at: string;
-  };
+
+enum StatusEnum {
+  REVISI = "revisi",
+  SELESAI = "selesai",
+  BARU = "baru",
+}
+
+interface User  {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
+  password: string;
+  remember_token: string;
+  role: string;
+  created_at: string;
+  updated_at: string;
+};
+
+
+interface PengumpulanPenugasan {
+  id: number;
+  penugasan_id: string;
+  link_google_drive: string;
+  user_id: string;
+  catatan: string;
+  status: string;
+  penugasan: Penugasan;
+  user: User
 }
 
 const PengumpulanPenugasan = () => {
+  const [catatan, setCatatan] = useState("");
   const [userRole, setUserRole] = useState<string | null>(null);
-  const [pengumpulanPenugasan, setPengumpulanPenugasan] = useState<pengumpulanPenugasan[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<StatusEnum>(StatusEnum.BARU);
+  const [pengumpulanPenugasan, setPengumpulanPenugasan] = useState<PengumpulanPenugasan[]>([]);
   const [showPopup, setShowPopup] = useState(false);
   const togglePopup = () => setShowPopup(!showPopup);
   const [link, setLink] = useState('');
@@ -53,7 +58,8 @@ const PengumpulanPenugasan = () => {
   const [userId, setUserId] = useState<number | null>(null);
   const router = useRouter();
   const token = Cookies.get("token");
-  const [penugasan, setPenugasan] = useState<penugasan[]>([]);
+  const [penugasan, setPenugasan] = useState<Penugasan[]>([]);
+  const params = useParams();
 
   useEffect(() => {
     if (token) {
@@ -69,8 +75,27 @@ const PengumpulanPenugasan = () => {
         .get("http://localhost:8000/api/penugasan")
         .then((response) => setPenugasan(response.data.data))
         .catch((error) => console.error("Error fetching pengumpulan penugasan items:", error));
+      
+      axios.get("http://localhost:8000/api/pengumpulan-penugasan")
+        .then(response => {
+          setPengumpulanPenugasan(response.data.data); // Ambil data pengumpulan penugasan
+        })
+        .catch(error => console.error("Error fetching pengumpulan penugasan data:", error));
     }
   }, [token]);
+
+  useEffect(() => {
+    const id = params.id;
+    if (id) {
+      axios
+        .get(`http://localhost:8000/api/pengumpulan-penugasan/${id}`)
+        .then((response) => {
+          const pengumpulanPenugasan = response.data.data;
+          setLink(pengumpulanPenugasan.link_google_drive);
+            })
+        .catch((error) => console.error("Error fetching pengumpulan-penugasan:", error));
+    }
+  }, [params.id]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -90,14 +115,7 @@ const PengumpulanPenugasan = () => {
 
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     axios
-  }, []);
-
-  useEffect(() => {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    axios
-      .get("http://localhost:8000/api/pengumpulan-penugasan")
-      .then((response) => setPengumpulanPenugasan(response.data.data))
-      .catch((error) => console.error("Error fetching paslon items:", error));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Submit form
@@ -109,13 +127,13 @@ const PengumpulanPenugasan = () => {
         link_google_drive: link,
         penugasan_id: selectedTaskId,
         user_id: userId,
-        status: 'baru'
+        status: 'baru',
       });
 
       alert('Link berhasil disimpan!');
       togglePopup();
     } catch (error) {
-      setError('Gagal menyimpan link, pastikan link valid.');
+      setError('Anda sudah mengerjakan tugas yang di berikan');
     }
   };
 
@@ -177,19 +195,50 @@ const PengumpulanPenugasan = () => {
                       <p className="text-black">
                         Keterangan: {penugasanItem.keterangan}
                       </p>
+                      {pengumpulanPenugasan.map((pengumpulanItem) =>
+                        pengumpulanItem.penugasan_id === penugasanItem.id ? (
+                          <p key={pengumpulanItem.id} className="text-black">
+                            Status: {pengumpulanItem.status}
+                          </p>
+                        ) : null
+                      )}
                     </div>
                     <div className="flex space-x-2">
-                    {userRole == "karyawan" && (
-                    <button
-                      className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
-                      onClick={() => {
-                        setSelectedTaskId(penugasanItem.id); // Simpan penugasan_id
-                        togglePopup();
-                      }}
-                    >
-                      Kerjakan
-                    </button>
-                    )}
+                      {userRole === "karyawan" && pengumpulanPenugasan.some((pengumpulanItem) =>
+                        parseInt(pengumpulanItem.penugasan_id) === penugasanItem.id
+                      ) ? (
+                        <button
+                          className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
+                          onClick={() => {
+                            setSelectedTaskId(penugasanItem.id);
+                            togglePopup();
+                          }}
+                        >
+                          Edit
+                        </button>
+                      ) : (
+                        <button
+                          className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
+                          onClick={() => {
+                            setSelectedTaskId(penugasanItem.id);
+                            togglePopup();
+                          }}
+                        >
+                          Kerjakan
+                        </button>
+                      )}
+
+                      {userRole === "superVisor" && (
+                        <button
+                          className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
+                          onClick={() => {
+                            setSelectedTaskId(penugasanItem.id);
+                            togglePopup();
+                          }}
+                        >
+                          Edit
+                        </button>
+                      )}
                     </div>
                   </li>
                 ))}
@@ -199,77 +248,84 @@ const PengumpulanPenugasan = () => {
                 Daftar Tugas Kosong.
               </p>
             )}
-              <div>
-              {pengumpulanPenugasan.length > 0 ? (
-                <ul>
-                  {pengumpulanPenugasan.map((pengumpulanPenugasanItem) => (
-                    <li
-                      key={pengumpulanPenugasanItem.id}
-                      className="bg-gray-50 p-4 mb-4 rounded-lg shadow-sm flex items-center justify-between"
-                    >
-                      <div className="flex flex-col">
-                        <p className="text-gray-600">
-                          Link Google Drive : {pengumpulanPenugasanItem.link_google_drive}
-                        </p>
-                        <p className="text-gray-600">
-                          User : {pengumpulanPenugasanItem.user.name}
-                        </p>
-                        <p className="text-gray-600">
-                          Catatan : {pengumpulanPenugasanItem.catatan}
-                        </p>
-                        <p className="text-gray-600">
-                          Status : {pengumpulanPenugasanItem.status}
-                        </p>
-                      </div>
-                      <div className="flex space-x-2">
-                        <Link href={`/pengumpulan-penugasan/${pengumpulanPenugasanItem.id}/edit`}>
-                          <button className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600">
-                            Edit
-                          </button>
-                        </Link>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="font-bold flex justify-center items-center">
-                  Daftar Tugas Kosong.
-                </p>
-              )}
-              </div>
           </div>
         </div>
         {showPopup && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <h2 className="text-xl mb-4">Masukkan Link Google Drive</h2>
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white p-6 rounded shadow-lg">
+              <h2 className="text-xl mb-4 text-center">
+                {userRole === "karyawan" ? "Masukkan atau Edit Link Google Drive" : "Edit Tugas"}
+              </h2>
+              
+              {error && <p className="text-red-500 mb-2 text-center">{error}</p>}
+              
+              <form onSubmit={handleSubmit}>
+                {/* Input untuk karyawan */}
+                {userRole === "karyawan" && (
+                  <input
+                    type="url"
+                    placeholder="Masukkan Link Google Drive"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
+                    className="border p-2 w-full mb-4"
+                    required
+                  />
+                )}
 
-            <form onSubmit={handleSubmit}>
-              <input
-                type="url"
-                placeholder="Masukkan Link Google Drive"
-                value={link}
-                onChange={(e) => setLink(e.target.value)}
-                className="border p-2 w-full mb-4"
-                required
-              />
-              {error && <p className="text-red-500">{error}</p>}
+                {/* Input untuk superVisor */}
+                {userRole === "superVisor" && (
+                  <div>
+                    <label
+                      htmlFor="status"
+                      className="block text-gray-700 font-medium mb-1"
+                    >
+                      Status
+                    </label>
+                    <select
+                      value={selectedStatus}
+                      onChange={(e) => setSelectedStatus(e.target.value as StatusEnum)}
+                      className="mb-4 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                    >
+                      <option value="">Pilih Status</option>
+                      {Object.values(StatusEnum).map((status) => (
+                        <option key={status} value={status}>
+                          {status}
+                        </option>
+                      ))}
+                    </select>
 
-              <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">
-                Simpan
-              </button>
+                    <label
+                      htmlFor="catatan"
+                      className="block text-gray-700 font-medium mb-1"
+                    >
+                      Catatan
+                    </label>
+                    <input
+                      id="catatan"
+                      type="text"
+                      value={catatan}
+                      onChange={(e) => setCatatan(e.target.value)}
+                      placeholder="Berikan Catatan jika diperlukan"
+                      className="mb-4 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                    />
+                  </div>
+                )}
+                
+                <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">
+                  Simpan
+                </button>
 
-              <button
-                type="button"
-                onClick={togglePopup}
-                className="bg-red-500 text-white px-4 py-2 rounded ml-4"
-              >
-                Batal
-              </button>
-            </form>
+                <button
+                  type="button"
+                  onClick={togglePopup}
+                  className="bg-red-500 text-white px-4 py-2 rounded ml-4"
+                >
+                  Batal
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
   );
 };
